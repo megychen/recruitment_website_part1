49c5a867b66867b51310088d2a3876c1192d7296
megy chen <megy.chen203@gmail.com>
2017-01-06 17:27:01 +0800

    add admin to users

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :require_is_admin
 
   def index
     @jobs = Job.all
